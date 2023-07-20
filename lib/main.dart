1d13de6c6200fa88a991840cb77272857d9c44d8
Himanshu Rawat <113584120+himanshu-rawat77@users.noreply.github.com>
2023-07-20 17:08:17 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:day2/login.dart';
+import 'package:day2/home.dart';
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: 'home',
+    routes: {
+      'home' : (context)=>Login(),
+      'login' : (context)=>HomePage()
+    },
+  ));
+
+}
